9499731edf9573738c3d338d971c04624a51d5df
browco <coralie.capron@hotmail.fr>
2019-11-19 14:55:52 +0100

    App_sklton : Uploading files v.3 : Uploading files and reading genes finally working for bitr (dirty)

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -1,4 +1,34 @@
 library(shiny)
+library(org.At.tair.db) #ataliana
+library(org.Ce.eg.db) #celegans
+library(org.Dm.eg.db) #dmelanogaster
+library(org.Dr.eg.db) #drerio
+library(org.EcK12.eg.db) #ecoli straink12
+library(org.Hs.eg.db) #hsapiens
+library(org.Mm.eg.db) #mmusculus
+library(org.Sc.sgd.db) #scerevisiae
+library(org.Xl.eg.db) #xenopus
+library(clusterProfiler)
+
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# 
+# BiocManager::install("org.Xl.eg.db")
+# BiocManager::install("org.At.tair.db")
+# BiocManager::install("org.Ce.eg.db")
+# a
+# BiocManager::install("org.Dm.eg.db")
+# a
+# BiocManager::install("org.Dr.eg.db")
+# a
+# BiocManager::install("org.EcK12.eg.db")
+# a
+# BiocManager::install("org.Hs.eg.db")
+# a
+# BiocManager::install("org.Mm.eg.db")
+# a
+# BiocManager::install("org.Sc.sgd.db")
+# a
 
 # Define UI for data upload app ----
 ui <- fluidPage(
@@ -18,8 +48,9 @@ ui <- fluidPage(
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")),
-      textAreaInput("manualEntry", "...Or write your list of genes", "geneID1, geneID2, etc.", width = "200px"),
-      verbatimTextOutput("value"),
+      textAreaInput("manualEntry", "...Or write your list of genes", width = "200px"),
+      helpText("Write a list of genes like this : geneID,geneID2,geneID3"),
+      verbatimTextOutput("value"), #Will allow the attribution of manualEntry in var output$value
 
       # Only show the file options if there is a file uploaded 
       conditionalPanel(
@@ -36,68 +67,132 @@ ui <- fluidPage(
                                Semicolon = ";",
                                Tab = "\t",
                                WhiteSpace = " "),
-                   selected = ",")
+                   selected = ","),
+      radioButtons("quote", "Quote",
+                   choices = c(None = "",
+                               "Double Quote" = '"',
+                               "Single Quote" = "'"),
+                   selected = '"')
+
       ),
-      # radioButtons("quote", "Quote",
-      #              choices = c(None = "",
-      #                          "Double Quote" = '"',
-      #                          "Single Quote" = "'"),
-      #              selected = '"'),
-      
-      #),
       # Horizontal line ----
       tags$hr(),
-      selectInput("organism", "Select organism(s)", list(
-        "Vertebrate"  = c("Human (Homo sapiens)"="human","Mouse (Mus musculus)"="mouse",
-                          "Western clawed frog (Xenopus tropicalis)"="xenopus","Zebrafish (Danio rerio)"="danio"
+      selectInput("organism", "Select organism", list(
+        "Vertebrate"  = c("Human (Homo sapiens)"="org.Hs.eg.db","Mouse (Mus musculus)"="org.Mm.eg.db",
+                          "Western clawed frog (Xenopus tropicalis)"="org.Xl.eg.db","Zebrafish (Danio rerio)"="org.Dr.eg.db"
                           ),
-        "Eukaryotic" = c("Fruit fly (Drosophila melanogaster)"="droso",
-                          "Nematode worm (Caenorhabditis elegans)"="celegans","Yeast (Saccharomyces cerevisiae)"="saccharomyces"),
-        "Prokaryotic" = c("Bacteria (Escherichia coli)"="ecoli")
-        
-      ), selected = NULL, multiple = TRUE)),
+        "Eukaryotic" = c("Fruit fly (Drosophila melanogaster)"="org.Dm.eg.db",
+                          "Nematode worm (Caenorhabditis elegans)"="org.Ce.eg.db","Yeast (Saccharomyces cerevisiae)"="org.Sc.sgd.db"),
+        "Prokaryotic" = c("Bacteria (Escherichia coli)"="org.EcK12.eg.db"),
+        "Plantae" = c("Angiosperma (Arabidopsis thaliana)"="org.At.tair.db")), selected = NULL, multiple = FALSE),
+      # Horizontal line ----
+      tags$hr(),
+      radioButtons("idType", "Gene identifier type",
+                   choices = c(ENTREZ = "ENTREZID",
+                               ENSEMBL = "ENSEMBL",
+                               SYMBOL = "SYMBOL",
+                               "COMMON NAME" = "COMMON"),
+                   selected = "ENSEMBL"),
+      # submit button
+      actionButton("submit", "Submit")),
     # Main panel for displaying outputs ----
       mainPanel(
       h3("This is the main title of the page"),
+       textOutput("orga")
       )
-      # Output: Data file ----
-      #tableOutput("contents")
-      
-    
   )
 )
-
-
 
 ####SERVER####################
 
 # Define server logic to read selected file ----
 server <- function(input, output) {
-  output$value <- renderText({input$manualEntry })
   output$file_listener <- reactive({
     return(!is.null(input$file))
   })
+  
   #Listener is always active 
   outputOptions(output, "file_listener", suspendWhenHidden = FALSE)
-  output$contents <- renderTable({
+  #  trim <- function (x) gsub("^\\s+|\\s+$", "", x) #remove trailing and leading whitespace
+  
+  observe({text_reactive()})
+  text_reactive <- eventReactive( input$submit, {
+
+   
+    ####Retrieve list of genes
+    geneList = reactive({
+      #Check if the user wants to upload a file or paste a list of genes
+      if(!is.null(input$file)){
+        geneList=read.csv(input$file$datapath,
+                          header = input$header,
+                          sep = input$sep,
+                          strip.white=TRUE,
+                          quote = input$quote
+        )
+        # y = data.frame(x = names(read.csv(input$file$datapath,
+        #                                        header = input$header,
+        #                                        sep = input$sep, 
+        #                                        strip.white=TRUE,
+        #                                        quote = input$quote
+        # )))
+        # y[] <- lapply(y, as.character)
+        # return(list('genes'=csv))
+        
+        # geneList = as.character(geneList[,1])
+        # geneList = sort(geneList, decreasing = TRUE)
+        return(list('data'=geneList))
+      }
+      else{
+        genes = c(strsplit(input$manualEntry, "[,\\t;  ]")) 
+        #will recognize any character used to write the list
+        return(list('data'=genes))
+      }
+    })
     
-    # input$file1 will be NULL initially. After the user selects
-    # and uploads a file, head of that data file by default,
-    # or all rows if selected, will be shown.
-    req(input$file)
-    #Check if the user wants to upload a file or paste a list of genes
-    if(is.null(input$file) == FALSE){
-      df <- read.csv(input$file$datapath,
-                     header = input$header,
-                     sep = input$sep)
-                     #quote = input$quote)
-      
-    }
-    else{
-      output$cleanValue = as.list(strsplit(output$value, ",")[[1]])
-      #renderPrint(output$cleanValue)
-    }
+  # Isolate the reactive values to store them and manipulate them
+  isolate({
+    ###Format of the IDs ?
+    req(geneList())
+    all_types = c(ENTREZ = "ENTREZID",
+                  ENSEMBL = "ENSEMBL",
+                  SYMBOL = "SYMBOL")
+
+
+    observe(print("GeneList"))
+    x= geneList()$data
+    vecgenes= x[[1]]
+    names(vecgenes)=x[[1]]
+    gene_list<-na.omit(vecgenes)
+    gene_list = sort(gene_list, decreasing = TRUE)
+    observe(print(gene_list))
+    observe(print(names(vecgenes)))
+
+    gene.df = bitr(names(vecgenes), fromType = input$idType,
+                   toType = c(all_types[!all_types %in% input$idType]),
+                   OrgDb = input$organism)
+
+    observe(print(gene.df))
+    observe(print(summary(gene.df)))
   })
-}
+  })
+}  
+  
+  
+  ### GO enrichments ------------
+  # GO group enrichment
+  # ggoDEGs_MF2 <- groupGO(gene = genesENS, OrgDb = database(), ont = "MF", level = 2, keyType = "ENSEMBL", readable = TRUE)
+  # barplot(ggoDEGs_MF2, showCategory = 30,  title = "GroupGO DEGs MF_2")
+  # 
+  # 
+  # ggoDEGs_BP2 <- groupGO(gene = genesENS, OrgDb = org.Hs.eg.db, ont = "BP", level = 2, keyType = "ENSEMBL", readable = TRUE)
+  # barplot(ggoDEGs_BP2, showCategory = 30,  title = "GroupGO DEGs BP_2")
+  #   
+  
+  
+  # })
+
+
+
+  
 # Run the app ----
 shinyApp(ui, server)
